97e1469d4e7529befe57c35338333009a527bd77
Muskan Kumari <muskansrivastava0305@gmail.com>
2024-10-09 12:06:07 +0530

    Book-store

--- Frontent/src/components/Footer.jsx
+++ Frontent/src/components/Footer.jsx
@@ -9,6 +9,7 @@ function Footer() {
           <a className="link link-hover">Contact</a>
           <a className="link link-hover">Jobs</a>
           <a className="link link-hover">Press kit</a>
+          
         </nav>
         <nav>
           <div className="grid grid-flow-col gap-4">
