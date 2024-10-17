6c5a7cc736e262c811beda0364c43ec61cd9699c
Muskan Kumari <muskansrivastava0305@gmail.com>
2024-10-17 18:30:40 +0530

    Bakend-Start

--- Backend/Index.js
+++ Backend/Index.js
@@ -9,10 +9,17 @@ dotenv.config();
 // const port = 3000
 
 const PORT = process.nextTick.PORT || 40001
+const URI = process.env.MongoDBURI;
 
-app.get('/', (req, res) => {
-  res.send('MERN kal ')
-})
+// app.get('/', (req, res) => {
+//   res.send('MERN kal ')
+// })
+
+try {
+    
+} catch (error) {
+    
+}
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
